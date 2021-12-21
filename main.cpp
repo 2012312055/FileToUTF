e87fa524747325de7dd44d9eb8e7089f83ff4ff1
unknown <esainment@gmail.com>
2021-12-21 22:28:40 +0900

    코드 수정

--- main.cpp
+++ main.cpp
@@ -6,17 +6,17 @@
 #include <string>
 #include <vector>
 #include <list>
-#include <utility>
 #include <fstream>
 #include <sstream>
-#include <future>
 
-[[nodiscard]] std::string GetCurrentPath()
+[[nodiscard]]
+std::string GetCurrentPath()
 {
 	return std::filesystem::current_path().string();
 }
 
-[[nodiscard]] std::list<std::filesystem::path> GetFilesRecursively(const std::string& current_path)
+[[nodiscard]]
+std::list<std::filesystem::path> GetFilesRecursively(const std::string& current_path)
 {
 	std::list<std::filesystem::path> files;
 
@@ -26,10 +26,11 @@
 		files.emplace_back(file.path());
 	}
 
-	return std::move(files);
+	return files;
 }
 
-[[nodiscard]] std::vector<std::string> GetExtensions(int argc, char* argv[])
+[[nodiscard]]
+std::vector<std::string> GetExtensions(int argc, char* argv[])
 {
 	std::vector<std::string> extensions;
 	extensions.reserve(argc - 2);
@@ -39,10 +40,11 @@
 		extensions.emplace_back(argv[i]);
 	}
 
-	return std::move(extensions);
+	return extensions;
 }
 
-[[nodiscard]] bool IsExtensionNeedToBeConverted(const std::string& input, const std::vector<std::string>& extensions)
+[[nodiscard]]
+bool IsExtensionNeedToBeConverted(const std::string& input, const std::vector<std::string>& extensions)
 {
 	for (const auto& extension : extensions)
 	{
@@ -55,7 +57,7 @@
 
 void ConvertFileToUTF8(const std::filesystem::path& file, const std::string& encoding_from)
 {
-	auto path = file.string();
+	auto path(file.string());
 
 	std::ifstream inf(path);
 	if (!inf.is_open())
@@ -70,8 +72,8 @@ void ConvertFileToUTF8(const std::filesystem::path& file, const std::string& enc
 	if (!outf.is_open())
 		return;
 
-	auto str = std::move(ss.str());
-	auto u8str = std::move(boost::locale::conv::to_utf<char>(str, encoding_from));
+	auto str(ss.str());
+	auto u8str(boost::locale::conv::to_utf<char>(str, encoding_from));
 
 	outf << u8str;
 
@@ -80,7 +82,7 @@ void ConvertFileToUTF8(const std::filesystem::path& file, const std::string& enc
 
 void ConvertFileWithInputExtensionToUTF8(const std::filesystem::path& file, const std::string& encoding_from, const std::vector<std::string>& extensions)
 {
-	auto extension = file.extension().string();
+	auto extension(file.extension().string());
 
 	if (!IsExtensionNeedToBeConverted(extension, extensions))
 		return;
@@ -90,19 +92,11 @@ void ConvertFileWithInputExtensionToUTF8(const std::filesystem::path& file, cons
 
 void ConvertFilesWithInputExtensionToUTF8(const std::list<std::filesystem::path>& files, const std::string& encoding_from, const std::vector<std::string>& extensions)
 {
-	std::size_t num_files = files.size();
-
-	std::vector<std::future<void>> converts;
-	converts.reserve(num_files);
+	std::size_t num_files(files.size());
 
 	for (const auto& file : files)
 	{
-		converts.emplace_back(std::async(ConvertFileWithInputExtensionToUTF8, file, encoding_from, extensions));
-	}
-
-	for (std::size_t i = 0; i < num_files; ++i)
-	{
-		converts[i].wait();
+		ConvertFileWithInputExtensionToUTF8(file, encoding_from, extensions);
 	}
 }
 
@@ -110,18 +104,20 @@ int main(int argc, char* argv[])
 {
 	if (argc < 3)
 	{
-		std::cout << R"(	Need encoding and extension. 
-	ex) %PROGRAM_NAME% EUC-KR .cpp .h)" << std::endl;
+		std::cout << "Need encoding and extension." << std::endl;
+		std::cout << "ex) filetoutf8 EUC-KR .cpp .h" << std::endl;
 		return EXIT_SUCCESS;
 	}
 
-	auto current_path = GetCurrentPath();
+	auto current_path(GetCurrentPath());
 
-	auto extensions = GetExtensions(argc, argv);
+	auto extensions(GetExtensions(argc, argv));
 
-	auto files = GetFilesRecursively(current_path);
+	auto files(GetFilesRecursively(current_path));
 
 	ConvertFilesWithInputExtensionToUTF8(files, argv[1], extensions);
+
+	std::cout << "DONE!" << std::endl;
 
 	return EXIT_SUCCESS;
 }
